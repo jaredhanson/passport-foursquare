42390fc4fadfa56d2d7ca3827082ce7f21a4899e
Jared Hanson <jaredhanson@gmail.com>
2014-03-08 19:01:21 -0800

    Add test case for profile parsing.

---
+++ test/profile.test.js
@@ -0,0 +1,31 @@
+/* global describe, it, expect, before */
+/* jshint expr: true */
+
+var fs = require('fs')
+  , parse = require('../lib/profile').parse;
+
+
+describe('profile.parse', function() {
+    
+  describe('example profile', function() {
+    var profile;
+    
+    before(function(done) {
+      fs.readFile('test/data/20120504.json', 'utf8', function(err, data) {
+        if (err) { return done(err); }
+        profile = parse(data);
+        done();
+      });
+    });
+    
+    it('should parse profile', function() {
+      expect(profile.id).to.equal('1419');
+      expect(profile.name.familyName).to.equal('Hanson');
+      expect(profile.name.givenName).to.equal('Jared');
+      expect(profile.gender).to.equal('male');
+      expect(profile.emails).to.have.length(1);
+      expect(profile.emails[0].value).to.equal('jaredhanson@example.com');
+    });
+  });
+  
+});
